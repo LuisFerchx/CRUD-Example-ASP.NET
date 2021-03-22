56a7b1a9909b00e400d2f7eeb8e0343839b477c7
LuisFerchx <luixg22@gmail.com>
2021-03-22 16:45:42 -0700

    primer commit

---
+++ Migrations/20210322180659_Initial.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace JudgeMyTaste.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FavoriteBands",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    EnteredBy = table.Column<string>(type: "text", nullable: true),
+                    EnteredOn = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FavoriteBands", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FavoriteBands");
+        }
+    }
+}
---
+++ Models/FavoriteBand.cs
@@ -0,0 +1,13 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace JudgeMyTaste.Models
+{
+  public class FavoriteBand
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string EnteredBy { get; set; }
+    public DateTime EnteredOn { get; set; }
+  }
+}
